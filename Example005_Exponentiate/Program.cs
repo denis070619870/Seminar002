96ed9aa6c1601d5b3fbb0d83998f77d293dda22b
Денис <denis070619870@rambler.ru>
2022-08-09 12:24:54 +0300

    Example005_Exponentiate

---
+++ Example005_Exponentiate/Program.cs
@@ -0,0 +1,16 @@
+﻿// Возведите число А в натуральную степень В используя цикл
+
+Console.WriteLine("Введите число, которое нужно возвести в степень: ");
+int A = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число, которое будет являться степенью: ");
+int B = int.Parse(Console.ReadLine());
+
+int count = 0;
+int result = 1;
+
+while (count < B)
+{
+    result = result * A;
+    count++;
+}
+Console.WriteLine(result);
