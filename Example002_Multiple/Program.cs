8afb695b1bc1bae55fa56ada99eb3aeb4cc6d345
Денис <denis070619870@rambler.ru>
2022-08-09 10:34:24 +0300

    Example002_Multiple

---
+++ Example002_Multiple/Program.cs
@@ -0,0 +1,12 @@
+﻿// Проверка числа на кратность 7 и 23
+
+Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+
+if (n % 7 == 0 && n % 23 == 0)
+{
+    Console.WriteLine($"Число {n} кратно 7 и 23");
+}
+else{
+    Console.WriteLine($"Число {n} не кратно 7 и 23");
+}
