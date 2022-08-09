54cab4fe066c09ec8f8168b524f2a06b3911d281
Денис <denis070619870@rambler.ru>
2022-08-09 12:09:54 +0300

    Example004_SquareNumber

---
+++ Example004_SquareNumber/Program.cs
@@ -0,0 +1,13 @@
+﻿// Вывести квадраты чисел от 1 до N
+
+Console.WriteLine("Введите число до которого будут посчитаны квадраты, начиная от 1: ");
+int N = int.Parse(Console.ReadLine());
+int count = 1;
+
+while (count <= N)
+{
+    Console.WriteLine(count * count);
+    count = count + 1;    
+}
+
+
