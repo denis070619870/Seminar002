6f506f23098ee80153201eb3007c125ee3c06a6c
Денис <denis070619870@rambler.ru>
2022-08-09 10:06:40 +0300

    Example001_QarterCoordinate

---
+++ Example001_QarterCoordinate/Program.cs
@@ -0,0 +1,23 @@
+﻿// Определить номер четверти плоскости, в которой находится точка с координатами X b Y неравными 0
+
+Console.WriteLine("Введите значение координат по оси X");
+int x = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите значение координат по оси Y");
+int y = int.Parse(Console.ReadLine());
+
+if (x>0 && y>0)
+{
+    Console.WriteLine("Точка в первой четверти");
+}
+else if (x>0 && y<0)
+{
+    Console.WriteLine("Точка во второй четверти");
+}
+else if (x<0 && y>0)
+{
+    Console.WriteLine("Точка в третьей четверти");
+}
+else if (x<0 && y<0)
+{
+    Console.WriteLine("Точка в первой четверти");
+}
